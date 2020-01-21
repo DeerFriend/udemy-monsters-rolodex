dcb03c4e064fb365fac59d591c6dad8bb820d499
DeerFriend <yaseminshn@gmail.com>
2020-01-21 11:17:12 +0000

    Updates

---
+++ precache-manifest.cbff5e3e73b1d077c3eff23c6ebb3e68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8946d0109ff388e623b6c7216531480f",
+    "url": "/udemy-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "89259dd8b6e4e515635d",
+    "url": "/udemy-monsters-rolodex/static/css/main.67114a3a.chunk.css"
+  },
+  {
+    "revision": "59c13745251206e2f1b0",
+    "url": "/udemy-monsters-rolodex/static/js/2.0729de66.chunk.js"
+  },
+  {
+    "revision": "89259dd8b6e4e515635d",
+    "url": "/udemy-monsters-rolodex/static/js/main.1150aef7.chunk.js"
+  },
+  {
+    "revision": "24c90becfb095b4aed4b",
+    "url": "/udemy-monsters-rolodex/static/js/runtime-main.27ed33f1.js"
+  }
+]);
